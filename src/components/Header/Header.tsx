0744b9b2e8bd314960f16f2672b6dcdd963aa6d5
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-09-04 12:13:29 +0300

    Добавляет компонент Slogan и его стили

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Logo from '../Logo/Logo';
+import Slogan from '../Slogan/Slogan';
 
 function Header() {
   return (
-    <div className='header mt-4'>
+    <div className='header'>
       <Logo />
+      <Slogan />
     </div>
   );
 }
