30e911efae0797f15112bddf27c5a30631de7d32
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-09-05 15:03:21 +0300

    Добавляет компонент CompanyActivities и его стили

---
+++ src/components/CompanyActivities/CompanyActivities.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function CompanyActivities() {
+  return (
+    <div className='company-activities'>
+      <div className='company-activities__decoration-box'>
+        <div className='company-activities__decoration-figure'></div>
+        <div className='company-activities__decoration-figure'></div>
+        <div className='company-activities__decoration-figure'></div>
+      </div>
+
+      <p className='company-activities__text'>
+        Проектирование, поставка и монтаж систем вентиляции и кондиционирования
+      </p>
+    </div>
+  );
+}
+
+export default CompanyActivities;
