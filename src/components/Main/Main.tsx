ce1041be9de97e9baa9b6d2ea97d2d464b1a484b
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-09-07 22:44:54 +0300

    Добавляет компонент Main

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ServicesProvided from './ServicesProvided/ServicesProvided';
+
+function Main() {
+  return (
+    <main className='main'>
+      <ServicesProvided />
+    </main>
+  );
+}
+
+export default Main;
