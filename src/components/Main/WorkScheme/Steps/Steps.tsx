b6dd4594bb2955200226fef607ace7d85021d770
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-10-22 21:55:27 +0300

    Добавляет компонент VisitObject

--- src/components/Main/WorkScheme/Steps/Steps.tsx
+++ src/components/Main/WorkScheme/Steps/Steps.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReceivingApplication from './ReceivingApplication/ReceivingApplication';
 import Consultation from './Consultation/Consultation';
+import VisitObject from './VisitObject/VisitObject';
 
 function Steps() {
   return (
     <div className='work-scheme__steps'>
       <ReceivingApplication />
       <Consultation />
+      <VisitObject />
     </div>
   );
 }
---
+++ src/components/Main/WorkScheme/Steps/VisitObject/VisitObject.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function VisitObject() {
+  return (
+    <div className='work-scheme__step work-scheme__visit-object'>
+      <h3 className='work-scheme__subtitle'>Выезд на объект</h3>
+      <p className='work-scheme__text'>
+        В назначенное время мы выезжаем на объект и согласовываем основные
+        технические решения, обсуждаем техническое задание
+      </p>
+    </div>
+  );
+}
+
+export default VisitObject;
