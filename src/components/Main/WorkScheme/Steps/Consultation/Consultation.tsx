dedcd71b3c6fa5c8fa98ca388bd170065874b018
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-10-22 20:35:25 +0300

    Добавляет компонент Consultation

---
+++ src/components/Main/WorkScheme/Steps/Consultation/Consultation.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Consultation() {
+  return (
+    <div className='work-scheme__step work-scheme__consultation'>
+      <h3 className='work-scheme__subtitle work-scheme__subtitle_align-right'>
+        Консультация
+      </h3>
+      <p className='work-scheme__text work-scheme__text_align-right'>
+        После обработки Вашей заявки, мы связываемся с Вами, выявляем
+        потребности и пожелания, а также проводим техническую консультацию
+      </p>
+    </div>
+  );
+}
+
+export default Consultation;
--- src/components/Main/WorkScheme/Steps/Steps.tsx
+++ src/components/Main/WorkScheme/Steps/Steps.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReceivingApplication from './ReceivingApplication/ReceivingApplication';
+import Consultation from './Consultation/Consultation';
 
 function Steps() {
   return (
-    <>
+    <div className='work-scheme__steps'>
       <ReceivingApplication />
-    </>
+      <Consultation />
+    </div>
   );
 }
 
