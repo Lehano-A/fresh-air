a4c04376da6fbb7de710d0826cb09a5034eabd58
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-11-03 16:12:16 +0300

    Добавляет компонент Portfolio

---
+++ src/components/Main/Portfolio/Portfolio.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Slider from './Slider/Slider';
+
+function Portfolio() {
+  return (
+    <section className='portfolio'>
+      <div className='portfolio__box-title-text'>
+        <h2 className='portfolio__title'>Портфолио и процесс работы</h2>
+        <p className='portfolio__text'>
+          Технические решения, принятые в рабочих чертежах, соответствуют
+          требованиям экологических, санитарно-гигиенических, противопожарных и
+          других норм и правил, и обеспечивают безопасную для жизни людей
+          эксплуатацию объекта при соблюдении предусмотренных рабочими чертежами
+          мероприятий.
+        </p>
+      </div>
+
+      <Slider />
+    </section>
+  );
+}
+
+export default Portfolio;
---
+++ src/components/Main/Portfolio/Slider/gallery.ts
@@ -0,0 +1,16 @@
+import supplyVentilation_1 from '../../../../images/Components/Slider/gallery/supply-ventilation-1.jpeg';
+import supplyVentilation_2 from '../../../../images/Components/Slider/gallery/supply-ventilation-2.jpeg';
+import ductedAirConditioner_1 from '../../../../images/Components/Slider/gallery/ducted-air-conditioner-1.jpeg';
+import ductedAirConditioner_2 from '../../../../images/Components/Slider/gallery/ducted-air-conditioner-2.jpeg';
+
+const gallery = [
+  { image: supplyVentilation_1 },
+  { image: supplyVentilation_2 },
+  { image: ductedAirConditioner_1 },
+  { image: ductedAirConditioner_2 },
+  { image: ductedAirConditioner_2 },
+  { image: ductedAirConditioner_2 },
+  { image: ductedAirConditioner_2 },
+];
+
+export default gallery;
