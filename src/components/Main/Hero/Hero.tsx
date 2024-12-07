4628f1a82f300cdb738b4e60792ab14099933133
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-12-07 18:00:11 +0300

    Добавляет логику изменения значения bottom для фона компонента Hero при изменении ширины экрана

--- src/components/Main/Hero/Hero.tsx
+++ src/components/Main/Hero/Hero.tsx
@@ -2,10 +2,81 @@ import Slogan from './Slogan/Slogan';
 import ButtonSubmit from '../../common/ButtonSubmit/ButtonSubmit';
 import CompanyActivities from './CompanyActivities/CompanyActivities';
 import Contacts from './Contacts/Contacts';
+import { useContext, useEffect, useRef } from 'react';
+import InnerWidthWindowContext from '../../../context/innerWidthWindowContext';
+import { regexDigit } from '../../../constants';
+
+const minBottom = -472;
+const maxBottom = -668;
 
 function Hero() {
+  const bgHeroRef = useRef<HTMLDivElement>(null);
+  const isFirstRender = useRef(true); // для strict mode
+  const { prevInnerWidth, innerWidth } = useContext(InnerWidthWindowContext);
+
+  // вычисление при первом рендере
+  useEffect(() => {
+    if (isFirstRender.current) {
+      calcAndSetBottom();
+    }
+  }, []);
+
+  // вычисления, при изменении ширины экрана
+  useEffect(() => {
+    if (prevInnerWidth > 0) {
+      calcAndSetBottom();
+    }
+  }, [innerWidth]);
+
+  // вычислить и установить bottom
+  function calcAndSetBottom() {
+    if (bgHeroRef.current) {
+      const newBottom = calcNewBottom();
+      bgHeroRef.current.style.bottom = `${newBottom}px`;
+    }
+  }
+
+  // вычислить новый bottom
+  function calcNewBottom() {
+    if (bgHeroRef.current) {
+      if (isFirstRender.current) {
+        if (innerWidth >= 375 && innerWidth <= 767) {
+          isFirstRender.current = false;
+          return minBottom - (innerWidth - 375) / 2;
+        }
+      }
+
+      if (innerWidth <= 374) {
+        return minBottom;
+      }
+
+      if (innerWidth >= 768) {
+        return maxBottom;
+      }
+
+      const currentBottom = getCurrentBottomElement(bgHeroRef);
+
+      if (innerWidth >= 375 && innerWidth <= 767 && currentBottom) {
+        return currentBottom - (innerWidth - prevInnerWidth) / 2;
+      }
+    }
+  }
+
+  // получить текущий bottom элемента
+  function getCurrentBottomElement(ref: React.RefObject<HTMLDivElement>) {
+    if (ref.current) {
+      return Number(
+        window.getComputedStyle(ref.current).bottom.replace(regexDigit, ''),
+      );
+    }
+  }
+
   return (
     <section className='hero mb-100'>
+      <div
+        className='hero__bg'
+        ref={bgHeroRef}
+      ></div>
       <div className='hero__wrapper'>
         <div className='hero__info'>
           <Slogan />
