6ac6aa45d1e97d000851f4a41ba29fd3162017e8
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-12-10 16:38:51 +0300

    Добавляет состояние и вычисление текущей внутренней ширины экрана

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,28 +2,46 @@ import React, { useEffect, useState } from 'react';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
-import CommonMarginLeftContext from '../../context/commonMarginLeftContext';
+import CommonMarginLeftContext from '../../context/CommonMarginLeftContext';
+import InnerWidthWindowContext from '../../context/InnerWidthWindowContext';
+import NavigationMenu from '../NavigationMenu/NavigationMenu';
 
 function App() {
   const [commonMarginLeft, setCommonMarginLeft] = useState('0');
+  const [innerWidthWindow, setInnerWidthWindow] = useState({
+    prevInnerWidth: 0,
+    innerWidth: window.innerWidth,
+  });
 
   useEffect(() => {
-    const getAndSetMarginLeft = () => {
-      const heroWrapper = document.querySelector('.hero__wrapper');
+    const getAndSet = {
+      commonMarginLeft: () => {
+        const heroWrapper = document.querySelector('.hero__wrapper');
 
-      if (heroWrapper) {
-        const calcedMarginLeft = getComputedStyle(heroWrapper).marginLeft;
-        setCommonMarginLeft(calcedMarginLeft);
-      }
+        if (heroWrapper) {
+          const calcedMarginLeft = getComputedStyle(heroWrapper).marginLeft;
+          setCommonMarginLeft(calcedMarginLeft);
+        }
+      },
+
+      innerWidthWindow: () => {
+        setInnerWidthWindow((prevState) => ({
+          prevInnerWidth:
+            prevState.innerWidth <= 375 ? 375 : prevState.innerWidth,
+          innerWidth: window.innerWidth >= 767 ? 767 : window.innerWidth,
+        }));
+      },
     };
 
     const listenResize = () => {
       window.addEventListener('resize', () => {
-        getAndSetMarginLeft();
+        getAndSet.commonMarginLeft();
+        getAndSet.innerWidthWindow();
       });
     };
 
-    getAndSetMarginLeft();
+    getAndSet.commonMarginLeft();
+    getAndSet.innerWidthWindow();
     listenResize();
 
     return () => listenResize();
@@ -31,9 +49,12 @@ function App() {
 
   return (
     <CommonMarginLeftContext.Provider value={commonMarginLeft}>
-      <Header />
-      <Main />
-      <Footer />
+      <InnerWidthWindowContext.Provider value={innerWidthWindow}>
+        <NavigationMenu />
+        <Header />
+        <Main />
+        <Footer />
+      </InnerWidthWindowContext.Provider>
     </CommonMarginLeftContext.Provider>
   );
 }
