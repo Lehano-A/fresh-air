888fc0d5cffcbfba7b3f99055f023e2f3cb8e3b6
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-12-26 15:00:35 +0300

    Добавляет медиа-запросы под все разрешения для компонента WorkScheme + рефакторит компонент Steps

--- src/components/Main/WorkScheme/Steps/Steps.tsx
+++ src/components/Main/WorkScheme/Steps/Steps.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import ReceivingApplication from './ReceivingApplication/ReceivingApplication';
-import Consultation from './Consultation/Consultation';
-import VisitObject from './VisitObject/VisitObject';
-import Graphic from './Graphic/Graphic';
-import Contract from './Contract/Contract';
-import DeliveryInstallation from './DeliveryInstallation/DeliveryInstallation';
-import SigningAct from './SigningAct/SigningAct';
+import dataSteps from '../dataSteps';
+
+interface stepKeys {
+  subtitle: string;
+  text: string | null;
+  className: string;
+}
 
 function Steps() {
   return (
     <div className='work-scheme__steps'>
-      <ReceivingApplication />
-      <Consultation />
-      <VisitObject />
-      <Graphic />
-      <Contract />
-      <DeliveryInstallation />
-      <SigningAct />
+      {dataSteps.map((step: stepKeys, id: number) => (
+        <div className={`work-scheme__step work-scheme__${step.className}`}>
+          <div
+            className={`work-scheme__content-box ${id % 2 !== 0 ? 'work-scheme__content-box_align-right' : ''}`}
+          >
+            {/*_align-right — выравниваем контент во втором столбце по правой стороне */}
+            <h3 className='work-scheme__subtitle'>{step.subtitle}</h3>
+            {step.text && <p className='work-scheme__text'>{step.text}</p>}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
