8e3f39cd1094609ad0d7a48e441fb730b12c50f2
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-12-27 17:37:30 +0300

    Добавляет атрибут key динамическому списку в компоненте WorkScheme

--- src/components/Main/WorkScheme/Steps/Steps.tsx
+++ src/components/Main/WorkScheme/Steps/Steps.tsx
@@ -11,7 +11,10 @@ function Steps() {
   return (
     <div className='work-scheme__steps'>
       {dataSteps.map((step: stepKeys, id: number) => (
-        <div className={`work-scheme__step work-scheme__${step.className}`}>
+        <div
+          key={step.subtitle + id}
+          className={`work-scheme__step work-scheme__${step.className}`}
+        >
           <div
             className={`work-scheme__content-box ${id % 2 !== 0 ? 'work-scheme__content-box_align-right' : ''}`}
           >
