64208e42585a876e5773dd7b0a06dacb35f23b4f
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-01-12 00:51:27 +0300

    Исправляет переполнение контейнера фоновым изображением из первого элемента + увеличивает высоту последнего элемента в компоненте WorkScheme

--- src/components/Main/WorkScheme/WorkScheme.tsx
+++ src/components/Main/WorkScheme/WorkScheme.tsx
@@ -3,7 +3,7 @@ import Steps from './Steps/Steps';
 
 function WorkScheme() {
   return (
-    <section className='work-scheme mb-100'>
+    <section className='work-scheme pb-100'>
       <div className='work-scheme__wrapper'>
         <h2 className='work-scheme__title'>
           Понятная и эффективная схема работы
