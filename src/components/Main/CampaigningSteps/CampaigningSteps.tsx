5663e55479c4ce1f60f1a0d3130bdbf36fa6819c
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-01-23 22:18:30 +0300

    Правит структуру и выравнивание компонента CampaigningSteps

--- src/components/Main/CampaigningSteps/CampaigningSteps.tsx
+++ src/components/Main/CampaigningSteps/CampaigningSteps.tsx
@@ -5,14 +5,18 @@ import ListAgitation from './ListAgitation/ListAgitation';
 function CampaigningSteps() {
   return (
     <section className='campaigning-steps'>
-      <h2 className='campaigning-steps__subtitle'>
-        <span className='campaigning-steps__subtitle-span'>3 простых шага</span>
-        к очищению воздуха
-      </h2>
+      <div className='campaigning-steps__wrapper'>
+        <h2 className='campaigning-steps__subtitle'>
+          <span className='campaigning-steps__subtitle-span'>
+            3 простых шага
+          </span>
+          к очищению воздуха
+        </h2>
 
-      <ListAgitation />
+        <ListAgitation />
 
-      <Steps />
+        <Steps />
+      </div>
     </section>
   );
 }
