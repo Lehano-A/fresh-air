4648c32dd1039182af9faa33c532279095db53af
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-02-02 14:20:53 +0300

    Добавляет валидацию инпутам в компоненте FormFeedback

--- src/components/common/FormFeedback/FormFeedback.tsx
+++ src/components/common/FormFeedback/FormFeedback.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ButtonAction, { ButtonActionProps } from '../ButtonAction/ButtonAction';
 import InputText from '../InputText/InputText';
 
@@ -12,8 +12,64 @@ interface FormFeedbackProps {
   outerBgColorSubmit?: string;
 }
 
+interface inputsValues {
+  name: { value: string; validation: boolean };
+  numberPhone: { value: string; validation: boolean };
+}
+
 function FormFeedback({ layout, buttonSubmitStyle }: FormFeedbackProps) {
-  const { size, outerBgColor, disabled = false } = buttonSubmitStyle || {};
+  const { size, outerBgColor } = buttonSubmitStyle || {};
+
+  const [inputsValues, setInputsValues] = useState<inputsValues>({
+    name: { value: '', validation: false },
+    numberPhone: { value: '', validation: false },
+  });
+  const [resultValidation, setResultValidation] = useState(false);
+
+  useEffect(() => {
+    const keys = Object.keys(inputsValues);
+    const isUnsuccessfulValidation = keys.some((key) => {
+      if (inputsValues[key as keyof typeof inputsValues].validation === false) {
+        return true;
+      }
+    });
+
+    setResultValidation(isUnsuccessfulValidation);
+  }, [inputsValues]);
+
+  // обработать инпут
+  function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    // если пустая (строка очищается)
+    if (value.length === 0) {
+      setInputsValues((prevState) => ({
+        ...prevState,
+        [name]: { value, validation: false },
+      }));
+    } else if (validate(value, name)) {
+      // если строка проходит валидацию
+      setInputsValues((prevState) => ({
+        ...prevState,
+        [name]: { value, validation: true },
+      }));
+    }
+  }
+
+  // валидировать значение инпута
+  function validate(value: string, inputName: string) {
+    const regexStr = /^[a-zа-я]+$/gim;
+    const regexNum = /^\+?[\d|\-|\s]*$/g;
+
+    if (inputName === 'name') {
+      return regexStr.test(value);
+    }
+
+    if (inputName === 'numberPhone') {
+      return regexNum.test(value);
+    }
+  }
 
   return (
     <form
@@ -22,17 +78,23 @@ function FormFeedback({ layout, buttonSubmitStyle }: FormFeedbackProps) {
       <InputText
         layout={layout}
         placeholder='Ваше имя'
+        name='name'
+        handleOnChange={handleInput}
+        value={inputsValues.name.value}
       />
       <InputText
         layout={layout}
         placeholder='Номер телефона'
+        name='numberPhone'
+        handleOnChange={handleInput}
+        value={inputsValues.numberPhone.value}
       />
 
       <ButtonAction
         layout={layout}
         name='отправить'
         size={size}
-        disabled={disabled}
+        disabled={resultValidation}
         outerBgColor={outerBgColor}
       />
     </form>
--- src/components/common/InputText/InputText.tsx
+++ src/components/common/InputText/InputText.tsx
@@ -3,13 +3,25 @@ import React from 'react';
 interface InputTextProps {
   placeholder: string;
   layout?: string;
+  value?: string;
+  name: string;
+  handleOnChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-function InputText({ placeholder, layout }: InputTextProps) {
+function InputText({
+  placeholder,
+  layout,
+  value,
+  name,
+  handleOnChange,
+}: InputTextProps) {
   return (
     <input
       className={`input-text ${layout ? `input-text_in-${layout}` : ''}`}
       placeholder={placeholder}
+      value={value}
+      name={name}
+      onChange={handleOnChange}
     />
   );
 }
