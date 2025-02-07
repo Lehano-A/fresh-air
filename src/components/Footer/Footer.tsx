bd9d4e75fc179612f2341d25e1ec4a161a7a8458
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-02-07 12:47:28 +0300

    Рефакторит стили списка контактов в Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -27,7 +27,7 @@ function Footer() {
         <div className='footer__contacts'>
           <h2 className='footer__contacts-subtitle'>Контакты</h2>
           <ul className='footer__contacts-list'>
-            <li className='footer__contacts-item'>
+            <li className='footer__contacts-item footer__contacts-item_icon_number-phone footer__contacts-item_border_bottom'>
               <a
                 className='footer__number-phone'
                 href='tel:+7492376331'
@@ -35,7 +35,7 @@ function Footer() {
                 + 7 492 373 63 31
               </a>
             </li>
-            <li className='footer__contacts-item'>
+            <li className='footer__contacts-item footer__contacts-item_icon_number-phone footer__contacts-item_border_bottom'>
               <a
                 className='footer__number-phone'
                 href='tel:+74243678929'
@@ -43,7 +43,7 @@ function Footer() {
                 + 7 424 367 89 29
               </a>
             </li>
-            <li className='footer__contacts-item'>
+            <li className='footer__contacts-item footer__contacts-item_icon_email footer__contacts-item_grid-position_email footer__contacts-item_border_bottom'>
               <a
                 className='footer__email'
                 href='mailto:PascalVent@mail.ru'
@@ -51,13 +51,13 @@ function Footer() {
                 PascalVent@mail.ru
               </a>
             </li>
-            <li className='footer__contacts-item footer__work-time'>
-              {`Пн - Сб: 9:00 - 19:00
-Вс: выходной`}
+            <li className='footer__contacts-item footer__contacts-item_icon_work-time footer__contacts-item_grid-position_work-time footer__contacts-item_padding-top_work-time'>
+              <span className='footer__work-time'>{`Пн - Сб: 9:00 - 19:00
+Вс: выходной`}</span>
             </li>
-            <li className='footer__contacts-item footer__address'>
-              {`Москва ул.
-              Плеханова, 28А`}
+            <li className='footer__contacts-item footer__contacts-item_icon_address footer__contacts-item_grid-position_address'>
+              <span className='footer__address'>{`Москва ул.
+              Плеханова, 28А`}</span>
             </li>
           </ul>
         </div>
